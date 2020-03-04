173bc925396278da3dcf0afabe2f8340571dc3f2
92497 <kyexkxl@126.com>
2020-03-04 14:52:08 +0800

    nihaonihao

--- src/Test.java
+++ src/Test.java
@@ -2,5 +2,7 @@ public class Test {
     public static void main(String args[]){
         System.out.println("test success");
         System.out.println("分支测试");
+        System.out.println("分支测试");
+        System.out.println("nihao" );
     }
 }
